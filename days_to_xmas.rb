b4aab28743abe42e241628afa531ac1320c86d2e
JoÃao Bernardo <joaogoncalob@gmail.com>
2022-04-07 00:21:17 +0100

    solved

--- days_to_xmas.rb
+++ days_to_xmas.rb
@@ -2,7 +2,11 @@ require "date"
 
 def days_to_xmas()
   # return the number o days until xmas
-  return (Date.new(Date.today.year,12,25)-Date.today).to_i
+  if Date.today <= Date.new(Date.today.year,12,25)
+    return (Date.new(Date.today.year,12,25)-Date.today).to_i
+  else
+    return (Date.new(Date.today.year + 1,12,25)-Date.today).to_i
+  end
 end
 
 puts days_to_xmas()
